7040c50545c06ffcb3c323b596670563ec08d604
Andrey1914 <anlikajw@gmail.com>
2023-12-24 20:16:40 +0100

    added dotenv config

--- corsOptions.js
+++ corsOptions.js
@@ -1,7 +1,5 @@
 const corsOptions = {
-  // origin: "https://budget-server-owpc.onrender.com",
   origin: "http://localhost:3000",
-
   credentials: true, // access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
--- db/db.js
+++ db/db.js
@@ -1,9 +1,13 @@
 const mongoose = require("mongoose");
 
+require("dotenv").config();
+
+const MONGO_URI = process.env.MONGO_URI;
+
 const db = async () => {
   try {
     mongoose.set("strictQuery", false);
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(MONGO_URI);
     console.log("MongoDB is connected succesfully!");
   } catch (error) {
     console.log("MongoDB is not connected.");
